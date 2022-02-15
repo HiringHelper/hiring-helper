f6eea2e6f5ba5a379aa4fa3e5c86bba7487ba42e
Ronak Hirpara <ronakh130@gmail.com>
2022-02-14 19:58:26 -0500

    Start drag and drop functionality

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import './Card.scss'
+import React from 'react';
+import './Card.scss';
 
 function Card(props) {
-  
   return (
-    <div className='card'>
+    <div className='card' ref={props.provided.innerRef} {...props.provided.draggableProps} {...props.provided.dragHandleProps}>
       <p className='card-title'>{props.title}</p>
       <em className='job-title'>Software Engineer</em>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,19 +1,53 @@
 import React from 'react';
 import './Column.scss';
 import Card from '../Card/Card.jsx';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
+import { useSelector } from 'react-redux';
+
 function Column(props) {
-  const cards = ['Chase', 'Facebook'];
+  // const cards = ['Chase', 'Facebook'];
+  // const cards2 = [
+  //   {
+  //     name: 'Chase',
+  //     title: 'Software Engineer',
+  //     status: 'Applied',
+  //   },
+  //   {
+  //     name: 'Facebook',
+  //     title: 'Software Engineer',
+  //     status: 'Wishlist',
+  //   },
+  //   {
+  //     name: 'Netflix',
+  //     title: 'Software Engineer',
+  //     status: 'Rejected',
+  //   },
+  // ];
+  const cards = useSelector((state) => state.jobs[props.title]);
   
+  const name = props.title + '-column';
+
   return (
     <div className='column'>
       <div className='column-title'>
         <p>{props.title}</p>
       </div>
-      <div className='card-container'>
-        {cards.map((ele) => (
-          <Card title={ele} />
-        ))}
-      </div>
+      <Droppable droppableId={name}>
+        {(providedDrop) => {
+          return (
+            <div className='card-container' {...providedDrop.droppableProps} ref={providedDrop.innerRef}>
+              {cards.map((ele, ind) => {
+                return (
+                  <Draggable key={ind} draggableId={'wishlist-' + ind + '-' + ele} index={ind}>
+                    {(providedDrag, snapshot) => <Card title={ele} ind={ind} provided={providedDrag} isDragging={snapshot.isDragging} />}
+                  </Draggable>
+                );
+              })}
+              {providedDrop.placeholder}
+            </div>
+          );
+        }}
+      </Droppable>
     </div>
   );
 }
