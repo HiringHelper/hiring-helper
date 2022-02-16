e54d422039aca9b97b757d73f22cc8f222825776
Ronak Hirpara <ronakh130@gmail.com>
2022-02-16 11:31:01 -0500

    Add routing back to login page from signup

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,34 +1,35 @@
 import React from 'react';
 import './LoginPage.scss';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+
 function LoginPage() {
   return (
     <div className='app'>
-    <>
-      <div id='login-background'>
-        <div id='title-container'>
-          <h1>App Name</h1>
+      <>
+        <div id='login-background'>
+          <div id='title-container'>
+            <h1>App Name</h1>
+          </div>
         </div>
-      </div>
-      <div id='login-parent'>
-        <div id='input-container'>
-          <form>
-            <label for='fname'>Username:</label>
-            <input type='text' id='fname' name='fname'></input>
-            <br />
-            <br />
-            <label for='lname'>Password:</label>
-            <input type='text' id='lname' name='lname'></input>
-            <br />
-            <br />
-            <input type='submit' value='Submit'></input>
-          </form>
-          <Link to='signup'>Signup</Link>
+        <div id='login-parent'>
+          <div id='input-container'>
+            <form>
+              <label for='fname'>Username:</label>
+              <input type='text' id='fname' name='fname'></input>
+              <br />
+              <br />
+              <label for='lname'>Password:</label>
+              <input type='text' id='lname' name='lname'></input>
+              <br />
+              <br />
+              <input type='submit' value='Submit'></input>
+            </form>
+            <Link to='signup'>Signup</Link>
 
-          <Link to='home'>To Home</Link>
+            <Link to='home'>To Home</Link>
+          </div>
         </div>
-      </div>
-    </>
+      </>
     </div>
   );
 }
